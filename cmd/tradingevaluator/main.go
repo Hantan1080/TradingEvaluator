2bd2c8b6572c8fad40a7d29ec48584721c2f3ff8
Hantan <tease@eteb.me>
2025-10-21 17:19:46 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/tradingevaluator/main.go
@@ -0,0 +1,23 @@
+// cmd/tradingevaluator/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "tradingevaluator/internal/tradingevaluator"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := tradingevaluator.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
